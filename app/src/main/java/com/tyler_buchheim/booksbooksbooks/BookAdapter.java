de634e9ba5146f0d5209ab6b6ea918aaebe02618
Tyler Buchheim <tbuchhei@alumni.nd.edu>
2017-10-29 19:30:27 -0500

    Create BookAdapter static placeholder info

--- app/src/main/java/com/tyler_buchheim/booksbooksbooks/BookAdapter.java
+++ app/src/main/java/com/tyler_buchheim/booksbooksbooks/BookAdapter.java
@@ -1,8 +1,34 @@
 package com.tyler_buchheim.booksbooksbooks;
 
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+
 /**
  * Created by buchh on 10/29/2017.
  */
 
-public class BookAdapter {
+public class BookAdapter extends ArrayAdapter<String> {
+
+    public BookAdapter(Activity context, ArrayList<String> books) {
+        super(context, 0, books);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item,
+                    parent, false);
+        }
+
+        return listItemView;
+    }
 }
--- app/src/main/java/com/tyler_buchheim/booksbooksbooks/MainActivity.java
+++ app/src/main/java/com/tyler_buchheim/booksbooksbooks/MainActivity.java
@@ -2,12 +2,27 @@ package com.tyler_buchheim.booksbooksbooks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BookAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<String> books = new ArrayList<String>();
+        books.add("dummy book");
+        books.add("dummy book");
+        books.add("dummy book");
+
+        ListView bookListView = (ListView) findViewById(R.id.list);
+        mAdapter = new BookAdapter(this, books);
+
+        bookListView.setAdapter(mAdapter);
     }
 }
